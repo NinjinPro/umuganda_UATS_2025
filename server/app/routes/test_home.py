3eac0151d4bcb12b1258d7128fc080589b95c9e2
NinjinPro <gracieuxdevelop@gmail.com>
2025-07-27 01:20:49 +0200

    Delete server/app/routes/test_home.py

--- server/app/routes/test_home.py
+++
@@ -1,9 +0,0 @@
-from app.models import User, Umuganda, Fine
-from flask import Blueprint, jsonify, render_template
-
-test_home_blue_print = Blueprint("home_blue_print", __name__)
-
-@test_home_blue_print.route("/home")
-@test_home_blue_print.route("/")
-def home_page():
-    return render_template("auth.html")
