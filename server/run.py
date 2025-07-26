6d6cdb2963ce0f85f48a94b642c2c4c29cbed3f1
NinjinPro <gracieuxdevelop@gmail.com>
2025-07-26 21:32:14 +0200

    Update run.py

--- server/run.py
+++ server/run.py
@@ -1,1 +1,10 @@
+from app import create_app
+import os
+app = create_app()
 
+if __name__ == '__main__':
+    # app.run("localhost", 1001, debug=True) # local
+    host = os.getenv("HOST", "127.0.0.1")
+    port = os.getenv("PORT", 5000)
+    debug = os.getenv("FLASK_DEBUG", "False") == "True"
+    app.run(host, port, debug)
